1779a5e38c72eb09ccf313991c1953123848fc0b
abalmasDA <abalmasit@gmail.com>
2023-06-06 19:49:11 +0300

    Initial commit (task1)

--- src/ua/itea/week10/task1/zoo/Main.java
+++ src/ua/itea/week10/task1/zoo/Main.java
@@ -1,7 +1,20 @@
+package ua.itea.week10.task1.zoo;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Zoo zoo = new Zoo();
+        zoo.addingAnimals("Horse");
+        zoo.addingAnimals("Shark");
+        zoo.addingAnimals("Bear");
+        zoo.addingAnimals("Giraffe");
+        zoo.addingAnimals("Crocodile");
+        zoo.addingAnimals("Hippopotamus");
+        zoo.addingAnimals("Fox");
+        zoo.addingAnimals("Wolf");
+
+        zoo.getInfoAllListAnimals();
 
 
     }
--- src/ua/itea/week10/task1/zoo/Zoo.java
+++ src/ua/itea/week10/task1/zoo/Zoo.java
@@ -1,2 +1,20 @@
-package ua.itea.week10.task1.zoo;public class Zoo {
+package ua.itea.week10.task1.zoo;
+
+import java.util.ArrayList;
+
+public class Zoo {
+    private ArrayList<String> listAnimals = new ArrayList<>();
+
+
+    public void addingAnimals(String animalToAdd) {
+        listAnimals.add(listAnimals.size(), animalToAdd);
+    }
+
+    public void getInfoAllListAnimals() {
+        for (int i = 0; i < listAnimals.size(); i++) {
+            System.out.println(listAnimals.get(i));
+        }
+
+    }
+
 }
